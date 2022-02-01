200127d1ace94c7685e1b89e21a9f672d402d422
lalomx <lalo0892@gmail.com>
2022-02-01 17:40:52 -0600

    add router

---
+++ routers/router.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const { Router } = express;
+
+const movies = [
+  { id: 1, name: 'Joker' }, 
+  { id: 2, name: 'Good Fellas' },
+  { id: 3, name: 'Matrix' }
+];
+
+const router = Router();
+
+router.get('/', (req, res) => { 
+  res.status(200).send(movies)
+})
+
+router.get('/:id', (req, res) => {
+  const movie = movies.find(m => m.id == req.params.id)
+  if (!movie) {
+    res.status(404).send("Movie not found")
+    return
+  }
+
+  res.send(movie)
+})
+
+router.post('/', (req, res) => {
+  const { id, name } = req.body
+
+  movies.push({
+    id,
+    name
+  })
+
+  res.sendStatus(201) // created
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+
+  const movie = movies.find(m => m.id == id)
+  if (!movie) {
+    res.status(404).send("Movie not found")
+    return
+  }
+
+  movie.name = name;
+  res.sendStatus(200)
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+
+  const movie = movies.find(m => m.id == id)
+  if (!movie) {
+    res.status(404).send("Movie not found")
+    return
+  }
+
+  const index = movies.indexOf(movie)
+  movies.splice(index, 1)
+
+  res.sendStatus(200)
+})
+
+module.exports = router
